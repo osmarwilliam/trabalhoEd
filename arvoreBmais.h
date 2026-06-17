41987d3262c6186800f894aafe9f68e20dfb13c1
william <will@osiwill.william>
2026-06-16 21:46:06 -0300

    subindo todos arquivos do banco de dados

---
+++ arvoreBmais.h
@@ -0,0 +1,28 @@
+#ifndef ARVORE_B_MAIS_H
+#define ARVORE_B_MAIS_H
+
+#include "structs.h"
+
+/* Inicializa a árvore com o grau mínimo t */
+void inicializar_arvore(int t);
+
+/* Inserções */
+void inserir_pessoa(Pessoa nova_pessoa);
+void inserir_filme(Filme novo_filme);
+
+/* Buscas */
+Registro *buscar_por_nome(const char *nome);
+Registro *buscar_por_id(int id);
+
+/* Impressão em formato de árvore */
+void imprimir_arvore(void);
+
+/* Remoções */
+void remover_registro(const char *nome);
+void remover_por_id(int id);
+
+/* Varredura completa das folhas (para consultas analíticas).
+ * Chama callback(reg) para cada Registro encontrado. */
+void varredura_folhas(void (*callback)(const Registro *));
+
+#endif /* ARVORE_B_MAIS_H */
